761f2c9d4cdd3c8ab56a05adcb0300fbeb22ad8a
Alvin Ayusa <alvinayusa@alvins-mbp.lan>
2018-02-28 21:55:04 -0800

    fixed buttons and gifs are now showing up

--- assets/javascript/giphyapp.js
+++ assets/javascript/giphyapp.js
@@ -33,55 +33,52 @@ $(document).ready(function () {
 
             var carBrandInput = $("#carBrands-input").val().trim()
             var newCarButton = $("#buttonSection")
-
+            
             // render new button for the new car brand
-            newCarButton.append("<button>" +carBrandInput+  "</button>")
+            newCarButton.append("<button class='car-btn' data-name = '"+carBrandInput+"' >" +carBrandInput+  "</button>")
             
             // add new car brand into the array
             carBrand.push(carBrandInput)
-
+        
             // clear input text box
             $("#carBrands-input").val(" ")
         })
 
     function displayGIFs () {
 
+        
+    }
+    
+    // this function appends the GIF for the initial buttons set in the HTML
+    $(document).on("click", ".car-btn", function(){
+
+        // var x = $(this).data("search")
+       
         var brands = $(this).attr("data-name")
+        // console.log(brands)
         var queryURL = "http://api.giphy.com/v1/gifs/search?q="+brands+"&api_key=zfsw6K8gNmu19gskqjynTaRS65B7R0SF&limit=10"
-            console.log(queryURL)
-        $.ajax({
+            // console.log(queryURL)
+       
+            $.ajax({
             url: queryURL,
             method: "GET",
+       
         }).then(function(response){
+            console.log(response.data[0])
+            
+            var gifSection = $("<div class = 'gifs'>").appendTo("#newCarBrands")
+    
+            for (var i = 0; i < response.data.length; i++){
+                // console.log(response.data.length)
+                gifSection.html("<p>Rating:" +response.data[i].rating+ "</p>")
+                gifSection.html("<img src = '" +response.data[i].images.downsized.url + "'>")
+                
+            }
 
-            var gifSection = $("<div class = 'gifs'>")
-
+          
         })
+    })
+    
 
-    }
-
-    // this function appends the GIF for the initial buttons set in the HTML
-    // $("button").on("click", function(){
-    //         var x = $(this).data("search")
-
-            // var queryURL = "http://api.giphy.com/v1/gifs/search?q="+x+"&api_key=zfsw6K8gNmu19gskqjynTaRS65B7R0SF&limit=10"
-    //         console.log(queryURL)
-
-    //         $.ajax({
-    //             url: queryURL,
-    //             method: "GET"
-    //         })
-    //         .done(function(response){
-    //             console.log(response.data[0])
-
-    //             for (var i = 0; i < response.data.length; i++){
-    //                 $('.GIFArea').prepend("<p>Rating:" +response.data[i].rating+ "</p>")
-    //                 $('.GIFArea').prepend("<img src = '" +response.data[i].images.downsized.url + "'>")
-    //             }
-
-    //         })
-    //     })
-
-
-    // ----- END of document.ready function ----- 
+// ----- END of document.ready function ----- 
 })
